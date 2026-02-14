20867231f8bda04d7007fbdc00e115939df1668b
Filippo Baruffaldi <filippo@baruffaldi.info>
2026-02-14 17:45:20 +0100

    Fix OVHClient status output formatting by adding newline characters

--- wg_ddns_configure.sh
+++ wg_ddns_configure.sh
@@ -70,9 +70,9 @@ while true; do
 
   info "OVHClient status:"
   if crontab -l 2>/dev/null | grep -q "${WG_DDNS_PATH}"; then
-      printf "${C_GREEN} *${C_RESET} status: started"
+      printf "${C_GREEN} *${C_RESET} status: started\n"
   else
-      printf "${C_GREEN} *${C_RESET} status: stopped"
+      printf "${C_GREEN} *${C_RESET} status: stopped\n"
   fi
 
   if [ ! -n "$AUTO_CHOICE" ]; then
